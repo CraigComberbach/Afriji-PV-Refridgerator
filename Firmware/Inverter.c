abbd55c653fbe4e7afb56d70ff0492dea7be6b3a
CraigComberbach <craig.comberbach@gmail.com>
2017-04-09 16:40:46 -0500

    See Previous Commit
    It removed this file...grumble grumble grumble

--- Firmware/Inverter.c
+++ Firmware/Inverter.c
@@ -22,7 +22,7 @@ Compiler: XC16 v1.26	IDE: MPLABx 3.30	Tool: ICD3	Computer: Intel Core2 Quad CPU 
 /*************   Magic  Numbers   ***************/
 #define	PERIOD			159
 #define	SIZE_OF_ARRAY	60
-#define	DEADBAND		8	//Period resolution is 62.5nS, 8 time divisions allows for the waveform to peak at 12VDC or decay to 0V (which is a very efficient turn on point) before 
+#define	DEADBAND		2	//Period resolution is 62.5nS; This is the delay between turning on/off one and turning on/off the other
 
 /*************    Enumeration     ***************/
 enum SINE_WAVE_STAGES
@@ -43,15 +43,8 @@ enum SINE_WAVE_STAGES
 int multiplier = 1;
 int divider = 1;
 int adder = 0;
-unsigned int inverterOnPeriod[] =
+unsigned int inverterOnPeriod[SIZE_OF_ARRAY] =
 {
-//10kHz @ 60 points (0-90º of a sine wave) Note: This series factors in the Rise/Fall times buffer
-//8,		51,		93,		135,	178,	220,	262,	303,	345,	386,
-//427,	468,	508,	548,	587,	626,	665,	703,	741,	778,
-//815,	851,	886,	921,	955,	988,	1021,	1053,	1084,	1115,
-//1144,	1173,	1201,	1228,	1255,	1280,	1305,	1328,	1351,	1373,
-//1394,	1413,	1432,	1450,	1467,	1483,	1497,	1511,	1524,	1535,
-//1546,	1555,	1563,	1570,	1577,	1582,	1585,	1588,	1590,	1591, 1591
 //10kHz @ 60 points (0-90º of a sine wave) Note: This series factors in the Rise/Fall times buffer AND zero crossing AND 95% max PWM
 //0,		39,		79,		119,	158,	198,	237,	276,	315,	354,
 //393,	431,	469,	507,	544,	581,	617,	653,	689,	724,
@@ -59,13 +52,14 @@ unsigned int inverterOnPeriod[] =
 //1074,	1101,	1128,	1155,	1180,	1205,	1228,	1251,	1273,	1295,
 //1315,	1334,	1353,	1371,	1387,	1403,	1418,	1431,	1444,	1456,
 //1467,	1477,	1485,	1493,	1500,	1506,	1510,	1514,	1516,	1518, 1518
-//100kHz @ 60 points (0-90º of a sine wave) Note: This series factors in the Rise/Fall times buffer AND zero crossing AND 90% max PWM
-1,		4,		8,		12,		15,		19,		23,		26,		30,		34,
-37,		41,		44,		48,		51,		55,		58,		62,		65,		68,
-72,		75,		78,		81,		84,		87,		90,		93,		96,		98,
-101,	104,	106,	108,	111,	113,	115,	118,	120,	122,
-123,	125,	127,	129,	130,	132,	133,	134,	136,	137,
-138,	139,	139,	140,	141,	141,	142,	142,	142,	142
+//100kHz @ 60 points (0-90º of a sine wave) Note: This series factors in the Rise/Fall times buffer AND zero crossing
+5,		8,		12,		16,		20,		24,		28,		32,		36,		40,
+43,		47,		51,		55,		58,		62,		66,		69,		73,		76,
+80,		83,		86,		90,		93,		96,		99,		102,	105,	108,
+111,	113,	116,	118,	121,	123,	126,	128,	130,	132,
+134,	136,	138,	139,	141,	142,	144,	145,	146,	147,
+148,	149,	150,	151,	151,	152,	152,	152,	152,	153
+
 };
 
 /*************Interrupt Prototypes***************/
@@ -165,24 +159,7 @@ void Inverter_Routine(unsigned long time_mS)
 {
 	static enum SINE_WAVE_STAGES stage = SINE_0_TO_90;
 	static int currentStep = 0;
-	Nop();
-	//The following code is an implementation of the TI App Note SLAA602 "800VA Pure Sine Wave Inverter?s Reference Design"
-	//Positive going voltage
-	//HOA	_/- - -...
-	//LOB	- -\_/-...
-	//HOB	_ _/-\_...
-	//LOA	-\_ _ _...
-
-	//Negative going voltage (Inverse of Positive waveform)
-	//HOA	-\_ _ _...
-	//LOB	_ _/-\_...
-	//HOB	- -\_/-...
-	//LOA	_/- - -...
-
-	//It looks like the following three rules will always be true
-	//1) Diagonals are the inverse of each other
-	//2) Both diagonals are unique
-	//3) Negative side is a direct inverse of positive
+	
 	switch(stage)
 	{
 		case SINE_0_TO_90:
@@ -190,8 +167,11 @@ void Inverter_Routine(unsigned long time_mS)
 
 			//Advance in step or state
 			++currentStep;
-			if(currentStep >= (SIZE_OF_ARRAY-1))
+			if(currentStep >= (SIZE_OF_ARRAY))
+			{
+				currentStep = SIZE_OF_ARRAY-1;
 				stage = SINE_90;
+			}
 			break;
 		case SINE_90:	//No special PWM event, only sampling is required
 			Trigger_A2D_Scan();
@@ -201,8 +181,13 @@ void Inverter_Routine(unsigned long time_mS)
 		case SINE_90_TO_180:
 			Positive_Sine(currentStep);
 
-			if(--currentStep <= 0)
+			//Advance in step or state
+			--currentStep;
+			if(--currentStep < 0)
+			{
+				currentStep = 0;
 				stage = SINE_180;
+			}
 			break;
 		case SINE_180:
 			Zero_Crossing();
@@ -216,8 +201,13 @@ void Inverter_Routine(unsigned long time_mS)
 		case SINE_180_TO_270:
 			Negative_Sine(currentStep);
 
-			if(++currentStep >= (SIZE_OF_ARRAY-1))
+			//Advance in step or state
+			++currentStep;
+			if(currentStep >= (SIZE_OF_ARRAY))
+			{
+				currentStep = SIZE_OF_ARRAY-1;
 				stage = SINE_270;
+			}
 			break;
 		case SINE_270:	//No special PWM event, only sampling is required
 			Trigger_A2D_Scan();
@@ -226,8 +216,13 @@ void Inverter_Routine(unsigned long time_mS)
 		case SINE_270_TO_360:
 			Negative_Sine(currentStep);
 
-			if(--currentStep <= 0)
+			//Advance in step or state
+			--currentStep;
+			if(--currentStep < 0)
+			{
+				currentStep = 0;
 				stage = SINE_360;
+			}
 			break;
 		case SINE_360:
 			Zero_Crossing();
@@ -242,21 +237,23 @@ void Inverter_Routine(unsigned long time_mS)
 			stage = SINE_0_TO_90;
 			break;
 		default://How did we get here?
-			#warning "Code not implemented"
-			//I don't know where I am so I am going to allow circulation on the bottom half and reset to the start of the waveform
-//			//LOA
-//			OC1R = ;
-//
-//			//HOB
-//			OC2R = ;
-//
-//			//HOA
-//			OC3R = ;
-//			OC3RS = ;
-//
-//			//LOB
-//			OC4R = ;
-//			OC4RS = ;
+			//Cycle current through the top FETs and prepare to star back at zero degrees
+			//HOA - 100% High
+			OC2R				= 0;
+			OC2RS				= PERIOD+1;
+
+			//HOB - 100% High
+			OC3R				= 0;
+			OC3RS				= PERIOD+1;
+
+			//LOB - 100% Low
+			OC4RS				= 0;
+			OC4R				= PERIOD+1;
+
+			//LOA - 100% Low
+			OC1RS				= 0;
+			OC1R				= PERIOD+1;
+
 			currentStep = 0;
 			stage = SINE_0_TO_90;
 			break;
@@ -272,30 +269,30 @@ void Inverter_Routine(unsigned long time_mS)
 //LOB	\_____/-----...____________
 void Positive_Sine(int step)
 {
+	//LOA - 100% Low
+	OC1RS				= 0;
+	OC1R				= PERIOD+1;
+
 	//HOA - 100% High
 	OC2R				= 0;
 	OC2RS				= PERIOD+1;
 
-	//LOA - 100% Low
-	OC1RS				= 0;
-	OC1R				= PERIOD+1;
+	//HOB - Circulating current
+	OC3R				= DEADBAND;
+	OC3RS				= PERIOD - (inverterOnPeriod[step]*multiplier)/divider - DEADBAND;
 
 	//LOB - Conducting current
 	OC4R				= PERIOD - (inverterOnPeriod[step]*multiplier)/divider;
 	OC4RS				= PERIOD;
 
-	//HOB - Circulating current
-	OC3R				= DEADBAND;
-	OC3RS				= OC4R - DEADBAND;
-
 //*****Above this line is Mikes waveform, below in my custom*****//
 //	//HOA - 100% High
-//	OC2R				= 0;
-//	OC2RS				= PERIOD+1;
-//
 //	//HOB - 100% Low
 //	OC3R				= PERIOD+1;
 //	OC3RS				= 0;
+//
+//	OC2R				= 0;
+//	OC2RS				= PERIOD+1;
 //
 //	//LOA - 100% Low
 //	OC1R				= PERIOD+1;
@@ -309,8 +306,8 @@ void Positive_Sine(int step)
 }
 
 //100% Low
-//	OCxR				= PERIOD+1;
 //	OCxRS				= 0;
+//	OCxR				= PERIOD+1;
 //	
 //100% High
 //	OCxR				= 0;
@@ -318,13 +315,13 @@ void Positive_Sine(int step)
 
 void Negative_Sine(int step)
 {
+	//HOA - Circulating Current
+	OC2R				= DEADBAND;
+	OC2RS				= PERIOD - (inverterOnPeriod[step]*multiplier)/divider - DEADBAND;
+
 	//LOA - Conducting Current
 	OC1R				= PERIOD - (inverterOnPeriod[step]*multiplier)/divider;
 	OC1RS				= PERIOD;
-
-	//HOA - Circulating Current
-	OC2R				= DEADBAND;
-	OC2RS				= OC1R - DEADBAND;
 
 	//LOB - 100% Low
 	OC4RS				= 0;
