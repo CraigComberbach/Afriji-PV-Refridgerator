18979cf33eb93a81e1321bba01c89e73df7ac9ac
CraigComberbach <craig.comberbach@gmail.com>
2017-04-26 18:52:02 -0500

    Frequency ramp up time is now 8 seconds
    By Mikes recommendation the code was modified to ramp the frequency up
    over 8 seconds instead of the 4 seconds we were previously doing

--- Firmware/main.c
+++ Firmware/main.c
@@ -54,7 +54,7 @@ int main(void)
 	Schedule_Task(HEART_BEAT_TASK,	&Heart_Beat_Task,	1000000/*uS Delay*/,	500000/*uS Period*/,	PERMANENT_TASK);
 	Schedule_Task(INVERTER_TASK,	&Inverter_Routine,	1000000/*uS Delay*/,	40/*uS Period*/,		PERMANENT_TASK);
 	Schedule_Task(A2D_TASK,			&A2D_Routine,		1000666/*uS Delay*/,	1000/*uS Period*/,		PERMANENT_TASK);//No longer than once ever 8mS will allow the result to be captured in time to be used with a 60Hz waveform
-	Schedule_Task(FREQUENCY_RAMP,	&Frequency_Ramp,	2000000/*uS Delay*/,	100000/*uS Period*/,	40/*Repetitions*/);
+	Schedule_Task(FREQUENCY_RAMP,	&Frequency_Ramp,	2000000/*uS Delay*/,	200000/*uS Period*/,	40/*Repetitions*/);
 
 	while(1)
 	{
