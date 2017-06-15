a04bdebc5622195de624c123bbc72b06778ec72b
Michael MacKay <mikemackay@mac.com>
2017-06-14 19:54:19 -0500

    Added exponential function
    Implemented exponential function. Implemented in expFunctionLookup.c and
    ".h

---
+++ Firmware/expFunctionLookup.h
@@ -0,0 +1,16 @@
+#ifndef EXPFUNCTIONLOOKUP_H
+#define	EXPFUNCTIONLOOKUP_H
+
+/*
+Instructions for adding to a new project:
+*/
+
+/***********Add to config file header************/
+/***************Add to config file***************/
+/************* Semantic Versioning***************/
+/*************   Magic  Numbers   ***************/
+/*************    Enumeration     ***************/
+/***********State Machine Definitions************/
+long exp(int x);
+
+#endif	/* SINEFUNCTIONLOOKUP_H */
